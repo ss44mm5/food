fa9b3387b9e0c418bfee917d6624e5a5f698d071
ss44mm5 <ss44mm5@gmail.com>
2026-03-11 03:14:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'food-app-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'food-swipe-v1';
+const ASSETS = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -12,7 +12,5 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
